44165b89d177f978bacffed5e7f1b5f4e11c7912
Regin8532 <138576075+Regin8532@users.noreply.github.com>
2025-10-24 10:26:06 +0530

    Delete Task_1 directory

--- Task_1/NumberPrinting.java
+++
@@ -1,9 +0,0 @@
-package Task_1;
-
-public class NumberPrinting {
-    public static void main(String[] args) {
-        for(int i=10;i<=50;i++){
-            System.out.println(i);
-        }
-    }
-}
